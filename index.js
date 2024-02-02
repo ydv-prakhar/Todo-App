519b08dcb562fdff59d7f910139c98925f0f895c
Prakhar Yadav <yadavprakhar1809@gmail.com>
2024-02-02 18:53:28 +0530

    Added the template for frontend

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
 const express = require("express");
  
 const app = express();
+
+app.set("view engine", "ejs");
+
+const todos = [{
+    todoId: "1",
+    todoTask: "Code",
+},
+{
+    todoId: "2",
+    todoTask: "Sleep",
+},
+{
+    todoId: "3",
+    todoTask: "Coffee",
+}
+];
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+app.get('/',(req,res)=>{
+    res.render('index',{
+        data:todos,
+    });
+});
+
+
